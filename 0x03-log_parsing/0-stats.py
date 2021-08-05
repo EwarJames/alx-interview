c331e0869bedd10ee7fb54f21d14ff911633240a
EwarJames <ewaarjames@gmail.com>
2021-08-05 14:53:01 +0000

    Python script that reads stdin line by line

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,32 +4,33 @@ import sys
 
 
 if __name__ == "__main__":
-    f_size, count = 0, 0
-    s_codes = ["200", "301", "400", "401", "403", "405", "500"]
-    status = [i: 0 for i in s_codes]
+    f_size, count = 0, 1
+    s = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "405": 0, "500": 0}
 
-    def print_stats(status: dict, f_size: int) -> None:
-        print("file size: {:d}".format(f_size))
-        for k, v in sorted(status.items()):
-            if v:
-                print("{}: {}".format(k, v))
+    def get_line(line):
+        """Parse and get data"""
+        try:
+            p_line = line.split()
+            s_code = p_line[-2]
+            if s_code in s.keys():
+                s[s_code] += 1
+            return int(p_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        print("file size: {}".format(f_size))
+        for k in sorted(s.items()):
+            if s[k]:
+                print("{}: {}".format(k, s[k])
 
     try:
         for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                s_code = data[-2]
-                if s_code in status:
-                    status[s_code] += 1
-            except BaseException:
-                pass
-            try:
-                f_size += int(data[-1])
-            except BaseException:
-                pass
+            f_size += get_line(line)
             if count % 10 == 0:
-                print_stats(status, f_size)
-        except KeyboardInterrupt:
-            print_stats(status, f_size)
-            raise
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
