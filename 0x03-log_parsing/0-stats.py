403468762219c728212470b2a05e121f94d9124f
EwarJames <ewaarjames@gmail.com>
2021-08-05 14:22:57 +0000

    Python script that reads stdin line by line

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     s_codes = ["200", "301", "400", "401", "403", "405", "500"]
     status = [i: 0 for i in s_codes]
 
-    def print_status(status: dict, f_size: int) -> None:
+    def print_stats(status: dict, f_size: int) -> None:
         print("file size: {:d}".format(f_size))
         for k, v in sorted(status.items()):
             if v:
@@ -29,7 +29,7 @@ if __name__ == "__main__":
             except BaseException:
                 pass
             if count % 10 == 0:
-                print_status(status, f_size)
+                print_stats(status, f_size)
         except KeyboardInterrupt:
-            print_status(status, f_size)
+            print_stats(status, f_size)
             raise
